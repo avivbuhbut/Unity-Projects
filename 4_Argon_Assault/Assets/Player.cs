6dfe6e823fd91368b32be7a85233c19e04edd465
avivbuhbut <avivb931@gmail.com>
2020-09-22 22:43:06 +0300

    22.09.20

--- 4_Argon_Assault/Assets/Player.cs
+++ 4_Argon_Assault/Assets/Player.cs
@@ -6,6 +6,8 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    [Tooltip("in ms")] [SerializeField] float Xspeed = 20f;
+    [Tooltip("in m")] [SerializeField] float Yspeed = 20f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +17,26 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
-       
+        float XThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // getting the current axis
+        float xOffset = XThrow * Xspeed * Time.deltaTime; // how many cm or meters i need to move in the frame 
+        print(xOffset);
+
+        float rawNewXPos = transform.localPosition.x + xOffset;
+     
+
+        /*MOVING LEFT AND RIGHT:
+         * change the x only of the  ship (to move left and right), z and y  stays as they are
+         *Mathf.clamp is a method to restrict the movemnt of one of the axis, in that case its the x*/
+        transform.localPosition = new Vector3(Mathf.Clamp(rawNewXPos, -10f, 10f), transform.localPosition.y,transform.localPosition.z);
+
+        /*Moving Up and Down*/
+
+        float YThrow = CrossPlatformInputManager.GetAxis("Vertical"); // getting the current axis
+        float yOffset = YThrow * Yspeed * Time.deltaTime; // Time.deltaTime is the time passed between the last frame to the current
+        print(yOffset);
+
+        float rawNewYPos = transform.localPosition.y + yOffset;
+
+        transform.localPosition = new Vector3(transform.localPosition.x, Mathf.Clamp(rawNewYPos, -10f, 10f), transform.localPosition.z);
     }
 }
