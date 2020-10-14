aa011570b06bf30bf651a7ca353a49299556689b
avivbuhbut <avivb931@gmail.com>
2020-10-14 22:29:36 +0300

    14.10.20 22:29

---
+++ MyGame/Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float hitPoints = 100f;
+    private Rigidbody2D rb;
+    
+
+    public ScObWeapon currentWeapon;
+    private float lastFired = 0f;       // last shot time
+    private float reloadTimer = 0f;     // time until reload finsihes
+    private int magazine;               // current bullet count
+    private bool reloading = false;      // is reloading
+
+    //public GameObject bulletPrefab;     // the prefab of our bullet
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        if (rb == null)
+        {
+            Debug.LogError("Player::Start cant find RigidBody2D </sadface>");
+        }
+        reloadTimer = currentWeapon.reloadSpeed;
+        magazine = currentWeapon.magazineCapacity;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            fire();     // do us a firing!
+        }
+        lastFired -= Time.deltaTime;        // reduce last fired Timer
+        if (reloading)
+        {                    // check if were reloading
+            reloadTimer -= Time.deltaTime;  // lower reload timer
+            if (reloadTimer <= 0)
+            {         // have we reloaded long enough
+                reloading = false;          // reset reload
+                magazine = currentWeapon.magazineCapacity;// refill our pew pew machine
+            }
+        }
+    }
+
+    // this is called at a fixed interval for use with physics objects like the RigidBody2D
+    void FixedUpdate()
+    {
+        // check if user has pressed some input keys
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+
+            // convert user input into world movement
+            float horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed;
+            float verticalMovement = Input.GetAxisRaw("Vertical") * moveSpeed;
+
+            //assign world movements to a Veoctor2
+            Vector2 directionOfMovement = new Vector2(horizontalMovement, verticalMovement);
+
+            // apply movement to player's transform
+            rb.AddForce(directionOfMovement);
+        }
+    }
+
+    private void startReload()
+    {
+        reloading = true;
+        reloadTimer = currentWeapon.reloadSpeed;
+    }
+
+    public void fire()
+    {
+        if (lastFired <= 0f && !reloading)
+        {    // make sure we dont shoot while reloading or too often
+            if (magazine == 1)
+            {    // this is our last bullet
+                startReload();      // start reloading
+            }
+            GameObject cameraObject = GameObject.Find("Main Camera");
+            Camera myCamera = cameraObject.GetComponent<Camera>();
+            Vector3 pointMouseVector = myCamera.ScreenToWorldPoint(Input.mousePosition);
+            pointMouseVector.z = 0; // set z to 0, this is 2D
+            GameObject go = Instantiate(currentWeapon.bulletPrefab, gameObject.transform.position, Quaternion.identity);
+            Bullet1 bullet = go.GetComponent<Bullet1>();
+            Vector3 targetVector = pointMouseVector - gameObject.transform.position;
+            bullet.targetVector = targetVector;
+            lastFired = currentWeapon.fireRate; // we just fired, add a delay with lastFired timer
+            magazine -= 1;  // bye bye bulletwdfdsf
+        }
+    }
+}
---
+++ MyGame/Assets/Scripts/PlayerMovments.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class PlayerMovments : MonoBehaviour
+{
+
+
+
+    public float jumpSpeed = 8f;
+
+    public float speed = 2f;
+
+    Rigidbody2D rigidbody2d;
+
+    public float moveSpeed = 5f;
+    public float hitPoints = 100f;
+    private Rigidbody2D rb;
+    public ScObWeapon currentWeapon;
+
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+
+        void FixedUpdate()
+        {
+
+            //Moving Left And RIgh
+            if (Input.GetKey(KeyCode.LeftArrow))
+                rigidbody2d.velocity = new Vector2(speed * -1, rigidbody2d.velocity.y);
+            if (Input.GetKey(KeyCode.RightArrow))
+                rigidbody2d.velocity = new Vector2(speed, rigidbody2d.velocity.y);
+
+            /*JUMPING*/
+            if (Input.GetButtonDown("Jump"))
+                rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, jumpSpeed);
+
+
+        }
+        // Start is called before the first frame update
+        //    void Start()
+        //  {
+
+        // }
+
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            FixedUpdate();
+
+
+           
+
+        }
+    }
